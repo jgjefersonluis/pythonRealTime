d75ab219aefa4e68e4e7c838dd05a8548073647c
jgjefersonluis <jgjefersonluis@gmail.com>
2022-06-17 10:51:46 -0300

    finalizado

--- realTime.py
+++ realTime.py
@@ -3,11 +3,30 @@ import random
 from  datetime import datetime
 from pytz import timezone
 
-
+fuso = timezone('America/Sao_Paulo')
+url = "https://api"
 calor = 0
 umidade = 0
 
 while calor < 100:
     calor += 1
+umidade = random.random()
+data = datetime.now(tz=fuso)
 
-print(calor)
+    
+payload = [
+    {
+        "UMIDADE": umidade,
+        "CALOR": calor,
+        "DATA": str(data)
+    }
+]
+r = requests.post(url, json=payload)
+
+time.sleep(2)
+
+        
+        
+
+
+# print(calor)
